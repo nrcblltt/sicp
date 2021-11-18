ca2e453758e8dbc0fbd5d624bf2f7f4afe0de20f
Marco Villotta <gmarcovil@gmail.com>
2021-11-18 22:56:20 +0100

    modify 2.76

--- ex02.76.scm
+++ ex02.76.scm
@@ -7,5 +7,5 @@
 ; 3 - new data objects. just write them, don't need to modify the other objects
 ; 3 - new operations. we modify the data objects who might receive the message (add the operation)
 
-; if new types of data objects are often added, 3 is better,
+; if new types of data objects are often added, 3 is better
 ; if new operations are often added, 2 is better.
