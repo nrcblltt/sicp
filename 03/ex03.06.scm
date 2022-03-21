29594501640a45ecfdd60ec07b0bd61dce31869b
Marco Villotta <gmarcovil@gmail.com>
2022-03-21 23:16:26 +0100

    add 3.6

---
+++ 03/ex03.06.scm
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define rand
+  (let ((x 234))
+    (lambda (m)
+      (cond ((eq? m 'generate)
+             (begin
+               (set! x (rand-update x))
+               x))
+            ((eq? m 'reset)
+             (lambda (new-value)
+               (set! x new-value)
+               x))
+            (else (error "option not available"))))))
+	     
+; a very poor rand-update
+(define (rand-update x)
+  (+ x 2))
+
+((rand 'reset) 18)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+((rand 'reset) 18)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
